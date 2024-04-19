8cdaa166267074cf8790291cbc6a9eac98c8d054
banzatto <banzatto@gmail.com>
2024-04-18 22:25:38 -0300

    Inclusão de paginação

---
+++ src/hooks/usePetList.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { GetPetsRequest, GetPetsResponse } from "../interfaces/pets";
+import { getPets } from "../services/pets/getPets";
+
+interface IUsePetList {
+    data?: GetPetsResponse
+    isLoading: boolean
+}
+
+export function usePetList(params: GetPetsRequest): IUsePetList {
+    const { data, isLoading } = useQuery({
+        queryKey: ['get-pets', params],
+        queryFn: () => getPets(params),  
+        
+    })
+
+    return {data, isLoading}
+}
--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid";
 import styles from './Pets.module.css'
@@ -7,15 +7,27 @@ import { Card } from "../../components/common/Card";
 import { getPets } from '../../services/pets/getPets';
 import { Skeleton } from '../../components/common/Skeleton';
 import { useQuery } from '@tanstack/react-query';
+import { Pagination } from '../../components/common/Pagination';
 
 
 
 export function Pets() {
+    const [searchParams, setSearchParams] = useSearchParams()
+    
+    const urlParams = {
+        page: searchParams.get('page') ? Number(searchParams.get('page')) : 1,
+    }
 
-    const { data, isLoading } = useQuery({
-        queryKey: ['get-pets'],
-        queryFn: () => getPets()
-    })
+    
+
+    
+
+    function changePage(page: number) {
+        setSearchParams((params) => {
+            params.set('page',String(page)) 
+            return params
+        })
+    }
 
     return (
         <Grid>
@@ -33,6 +45,11 @@ export function Pets() {
                         />
                     ))}
                 </main>
+                {data?.currentPage && (<Pagination currentPage={data.currentPage}
+                totalPages={data.totalPages}
+            onPageChange={(number) => changePage(number)}
+            />)
+            }
             </div>
         </Grid>
     )
