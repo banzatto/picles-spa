9197b7b8c512d77c4901117f9daa1e4724333bc8
banzatto <banzatto@gmail.com>
2024-04-23 22:24:31 -0300

    Adiconado component input, instalacao de bibliotecas react-hook-form zod @hookform/resolvers

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,6 +1,24 @@
+import { FormEvent } from "react";
+import { Button, ButtonVariant } from "../../../components/common/Button";
+import { Input } from "../../../components/common/Input/Input";
 import { Panel } from "../../../components/layout/Panel";
+import styles from "./Shelter.module.css"
 
 
-export function Shelter () {
-    return <Panel>Formulário de abrigo</Panel>
+export function Shelter() {
+
+    function submit(event) {
+        event.preventDefault()
+        console.log()
+    }
+
+    return <Panel>
+        <form className={styles.container} onSubmit={submit}>
+            <Input label="Nome" />
+            <Input label="WhatsApp" />
+            <Input label="Phone" />
+            <Input label="Email" />
+            <Button type="submit" variant={ButtonVariant.Default} >Salvar dados</Button> 
+        </form>
+    </Panel>
 }
